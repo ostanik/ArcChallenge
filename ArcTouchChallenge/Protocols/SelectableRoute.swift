f4a70b6335ddc92f388a3fbf755781536d2540b5
Ostanik <alan.ostanik@socialbase.com.br>
2016-10-30 13:34:23 -0200

    Creating a protocol to help the keep clean code on tableviews, and protocol to all details viewControllers.

---
+++ ArcTouchChallenge/Protocols/SelectableRoute.swift
@@ -0,0 +1,12 @@
+//
+//  SelectedGroupProtocol.swift
+//  ArcTouchChallenge
+//
+//  Created by Ostanik on 30/10/16.
+//  Copyright © 2016 Ostanik. All rights reserved.
+//
+
+import Foundation
+protocol SelectableRoute {
+    var selectedRoute : Route? {get set}
+}
